a4a951b9607e9f69468256c06f7ff6531197232a
Sophia Liu <sophialiu7436@gmail.com>
2023-06-17 21:14:22 -0500

    Initial Commit

---
+++ sketching-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sketching-app
+//
+//  Created by Sophia Liu on 6/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
