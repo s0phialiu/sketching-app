20d18a21269d5a58ec15283efc4fdddd6ecb81b4
Sophia Liu <sophialiu7436@gmail.com>
2023-06-17 22:43:58 -0500

    Added PKToolPicker functionality

--- sketching-app/ViewController.swift
+++ sketching-app/ViewController.swift
@@ -6,15 +6,56 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    
+    @IBOutlet weak var takeSaveButton: UIBarButtonItem!
+    @IBOutlet weak var inputToggleButton: UIBarButtonItem!
+    
+    private let canvasView: PKCanvasView = {
+        let canvas = PKCanvasView()
+        canvas.drawingPolicy = .anyInput // Allow drawing policy to be any input
+        return canvas
+    }()
+    
+    let drawing = PKDrawing() // To save drawing
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello")
+        canvasView.delegate = self
+        canvasView.drawing = drawing // Render image once the user saves it
+        view.addSubview(canvasView)
     }
     
+    // Set frame
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        canvasView.frame = view.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let toolPicker = PKToolPicker()
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+    }
+    
+    // Canvas functions and breakpoints
+    @objc func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        
+    }
+    @objc func canvasViewDidBeginUsingTool(_ canvasView: PKCanvasView) {
+        
+    }
+    @objc func canvasViewDidEndUsingTool(_ canvasView: PKCanvasView) {
+        
+    }
+    @objc func canvasViewDidFinishRendering(_ canvasView: PKCanvasView) {
+        
+    }
 
 
 }
